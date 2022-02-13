05fb99bba5802529245f72da715c5e442bd1dc2f
Hillaru <alaureenity@mail.ru>
2022-02-14 03:00:33 +0700

    fixed connection problem

--- hw2/lib/serv.ex
+++ hw2/lib/serv.ex
@@ -6,42 +6,38 @@ defmodule Serv do
   @doc """
   Nothing.
   """
-  
+
   def main() do
-      {:ok, db} = MyXQL.start_link(username: "hillaru", database: "test_db")
-      cycle(db)
+    {:ok, db} = MyXQL.start_link(username: "hillaru", database: "test_db", protocol: :tcp)
+    cycle(db)
   end
 
   defp cycle(db) do
-      id = IO.gets("Enter staff id: ") |> String.trim
-      try do
-        one_staff(db, id) |> output()
-      rescue
-        e -> IO.puts("Error")
-      end
-      cycle(db)
+    id = IO.gets("Enter staff id: ") |> String.trim
+    one_staff(db, id) |> output()
+    cycle(db)
   end
 
   def all_staff(db) do
-      {ok, data} = MyXQL.query(db, "SELECT * FROM staff")
-      [data.columns, data.rows]
+    {ok, data} = MyXQL.query(db, "SELECT * FROM staff")
+    [data.columns, data.rows]
   end
 
   def one_staff(db, id) do
-      {ok, data} = MyXQL.query(db, "SELECT * FROM staff WHERE Staff_id = ?", [id])
-      [data.columns, data.rows]
+    {ok, data} = MyXQL.query(db, "SELECT * FROM staff WHERE Staff_id = ?", [id])
+    [data.columns, data.rows]
   end
 
-  def output([[Id, FName, LName, Patr, Pass, Phone], data]) do
-      IO.puts("#{Id}, #{FName}, #{LName}, #{Patr}, #{Pass}, #{Phone}")
-      output_data(data)
+  def output([[id, fName, lName, patr, pass, phone], data]) do
+    IO.puts("#{id}, #{fName}, #{lName}, #{patr}, #{pass}, #{phone}")
+    output_data(data)
   end
-  
+
   defp output_data([]) do
-      :ok
+    :ok
   end
-  defp output_data([[Id, FName, LName, Patr, Pass, Phone] | tail]) do
-      IO.puts("#{Id}, #{FName}, #{LName}, #{Patr}, #{Pass}, #{Phone}")
-      output_data(tail)
+  defp output_data([[id, fName, lName, patr, pass, phone] | tail]) do
+    IO.puts("#{id}, #{fName}, #{lName}, #{patr}, #{pass}, #{phone}")
+    output_data(tail)
   end
 end
